c0886a1425e8cacc0dd5bf977aa7abe6287b548b
iryna.cherhina <iryna.cherhina@sigma.software>
2019-11-04 14:36:56 +0200

    proj => AwesomeTetrisGame; remove e2e settings

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should have as title \'proj\'', () => {
+  it('should have as title \'AwesomeTetrisGame\'', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('proj');
+    expect(app.title).toEqual('AwesomeTetrisGame');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('proj app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('AwesomeTetrisGameapp is running!');
   });
 });
