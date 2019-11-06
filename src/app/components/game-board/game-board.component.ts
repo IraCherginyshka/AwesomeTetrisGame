8c3d1f10fcdded60f2a0758dbdab5d884222f183
IraCherginyshka <39628469+IraCherginyshka@users.noreply.github.com>
2019-11-06 11:48:31 +0200

    Feature/ATG-10 Create main field with general styles (#6)
    * Create components (Header, Board, Option, Controls, Information, Leaderboard). Add icons to images folder.
    
    * Create components (Header, Board, Option, Controls, Information, Leaderboard). Add icons to images folder.
    
    * Add variables to styles
    
    * Resolve conversation. Comments.
    
    * Fix EsLint
    
    * Delete .prettierrc.js
    
    Delete unnecessary file

---
+++ src/app/components/game-board/game-board.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'atg-game-board',
+  templateUrl: './game-board.component.html',
+  styleUrls: ['./game-board.component.scss'],
+})
+export class GameBoardComponent implements OnInit {
+  ngOnInit() {
+  }
+}
---
+++ src/app/components/game-information/game-information.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'atg-game-information',
+  templateUrl: './game-information.component.html',
+  styleUrls: ['./game-information.component.scss'],
+})
+export class GameInformationComponent implements OnInit {
+  ngOnInit() {
+  }
+}
