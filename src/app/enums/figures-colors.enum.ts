ad2b40fa79aaf1c0ac8ea34452fbd0656fa44b14
iryna.cherhina <iryna.cherhina@sigma.software>
2019-11-15 10:11:56 +0200

    Fix export. Rename files

---
+++ src/app/enums/figures-colors.enum.ts
@@ -0,0 +1,10 @@
+export enum FiguresColors {
+  DEFAULT = '#2b3a42',
+  FIRST = '#fd0404',
+  SECOND = '#2e36d5',
+  THIRD = '#19af09',
+  FOURTH = '#f8ab05',
+  FIFTH = '#13e4e7',
+  SIXTH = '#ff7300',
+  SEVENTH = '#d131c4',
+}
