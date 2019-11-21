4b9d93b7f22b1412672e07285aac84564a715a24
IraCherginyshka <39628469+IraCherginyshka@users.noreply.github.com>
2019-11-21 15:54:35 +0200

    Feature/ATG-68 Service for game state  (#25)
    * Create game-state service for whole project
    Management information about state of game
    Pause/Reset buttons
    Change folders for controls
    
    * unsubscribe
    
    * Fixed code
    
    * Private method
    
    * fix return drawBoard()

--- src/app/components/game-board/game-board.component.ts
+++ src/app/components/game-board/game-board.component.ts
@@ -1,24 +1,32 @@
-import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { FigureModel } from '../../models/figure.model';
 import { BlockModel } from '../../models/block.model';
-import {
-  QUANTITY_BLOCKS_WIDTH,
-  QUANTITY_BLOCKS_HEIGHT,
-  DELAY_FIRST_LEVEL,
-  CANVAS_WIDTH,
-  CANVAS_HEIGHT,
-} from '../../constants/board-component.const';
 import { FiguresColors } from '../../enums/figures-colors.enum';
+import { GameStateService } from '../../services/game-state.service';
+import { GameState } from '../../enums/game-state.enum';
+import {
+  CANVAS_HEIGHT,
+  CANVAS_WIDTH,
+  DELAY_FIRST_LEVEL,
+  QUANTITY_BLOCKS_HEIGHT,
+  QUANTITY_BLOCKS_WIDTH,
+} from '../../constants/board-component.const';
 
 @Component({
   selector: 'atg-game-board',
   templateUrl: './game-board.component.html',
   styleUrls: ['./game-board.component.scss'],
 })
-export class GameBoardComponent implements OnInit {
+export class GameBoardComponent implements OnInit, OnDestroy {
   @ViewChild('canvas', { static: true }) private canvas: ElementRef<HTMLCanvasElement>;
   private ctx: CanvasRenderingContext2D;
   private boardMatrix: FiguresColors[][];
+  private subscriptionState: Subscription;
+  private timeInterval: number;
+  private lineWithFigure: number;
+
+  constructor(private gameStateService: GameStateService) {}
 
   ngOnInit(): void {
     this.canvas.nativeElement.width = CANVAS_WIDTH;
@@ -29,29 +37,54 @@ export class GameBoardComponent implements OnInit {
       QUANTITY_BLOCKS_HEIGHT,
     );
     this.play();
+    this.subscriptionState = this.gameStateService
+      .getGameState()
+      .subscribe((gameState: GameState) => {
+        if (gameState === GameState.RESET) {
+          this.reset();
+        }
+        if (gameState === GameState.PAUSE) {
+          this.pause();
+        }
+      });
   }
 
-  private static makeBoardEmptyMatrix(width: number, height: number): FiguresColors[][] {
+  ngOnDestroy(): void {
+    this.subscriptionState.unsubscribe();
+  }
+
+  static makeBoardEmptyMatrix(width: number, height: number): FiguresColors[][] {
     return new Array(height).fill(new Array(width).fill(FiguresColors.DEFAULT));
-  }
-
-  private play(): void {
-    let height = 0;
-    const newFigure = new FigureModel();
-    const itemHeight = newFigure.figureMatrix.length;
-    const interval = setInterval(() => {
-      this.drawBoard(newFigure.showFigure(height, this.boardMatrix));
-      if (height + itemHeight === QUANTITY_BLOCKS_HEIGHT) {
-        clearInterval(interval);
-        this.play();
-      }
-      height += 1;
-    }, DELAY_FIRST_LEVEL);
   }
 
   private drawBoard(matrix: FiguresColors[][]): void {
     matrix.forEach((line, indexY) => {
       line.forEach((item, indexX) => new BlockModel(this.ctx, item).fillBoardBlock(indexX, indexY));
     });
+  }
+
+  private play(): void {
+    this.lineWithFigure = 0;
+    const newFigure = new FigureModel();
+    const itemHeight = newFigure.figureMatrix.length;
+
+    this.timeInterval = window.setInterval(() => {
+      this.drawBoard(newFigure.showFigure(this.lineWithFigure, this.boardMatrix));
+      if (this.lineWithFigure + itemHeight === QUANTITY_BLOCKS_HEIGHT) {
+        clearInterval(this.timeInterval);
+        this.play();
+      } else {
+        this.lineWithFigure += 1;
+      }
+    }, DELAY_FIRST_LEVEL);
+  }
+
+  private pause(): void {
+    clearInterval(this.timeInterval);
+  }
+
+  private reset(): void {
+    clearInterval(this.timeInterval);
+    this.play();
   }
 }
