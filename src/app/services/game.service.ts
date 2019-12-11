3b6f4078ab7f3194552e11c048d3b35850f357b8
IraCherginyshka <39628469+IraCherginyshka@users.noreply.github.com>
2019-12-11 12:31:41 +0200

    Feature/ATG-34 Increase Level  (#46)
    * Update information about current level => increase speed
    
    Improve Reset game
    
    Use async pipes in information component
    
    * Resolve conversation
    private method => public
    use BehaviorSubject
    create Interface for gameStats

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { FiguresMovement } from '../enums/figures-movement.enum';
 import { GameState } from '../enums/game-state.enum';
 import { FigureModel } from '../models/figure.model';
 import { FiguresColors } from '../enums/figures-colors.enum';
+import {
+  DEFAULT_LINE_SCORE,
+  DEFAULT_STEP,
+  GAME_STEP_LEVEL,
+} from '../constants/game-information.const';
+import { GameStatsObject } from '../interfaces/gameStats.interface';
 
 @Injectable({ providedIn: 'root' })
 export class GameService {
@@ -11,17 +17,28 @@ export class GameService {
   private isLostGame = false;
   private currentFigure: FiguresColors[][];
   private nextFigure = FigureModel.getRandomFigure();
+  private currentLevel = 1;
+  private currentNumberLines = 0;
+  private currentScore = 0;
   private movementSubject = new Subject<FiguresMovement>();
   private gameStateSubject = new Subject<GameState>();
   private nextFigureSubject = new Subject<{
     previousFigure: FiguresColors[][];
     randomNextFigure: FiguresColors[][];
   }>();
-  private numberLinesSubject = new Subject<{
-    lines: number;
-    score: number;
-  }>();
+  private numberLinesSubject = new BehaviorSubject({
+    lines: 0,
+    score: 0,
+    level: 1,
+  });
   private lostGameSubject = new Subject<boolean>();
+
+  public static calculateScore(lines: number, level: number): number {
+    return (
+      (lines / 2) *
+      (2 * (DEFAULT_LINE_SCORE + DEFAULT_LINE_SCORE * level) + DEFAULT_STEP * (lines - 1))
+    );
+  }
 
   public setLostGame(): void {
     this.isLostGame = true;
@@ -70,19 +87,35 @@ export class GameService {
     return this.nextFigureSubject.asObservable();
   }
 
-  public setNumberFilledLines(lines: number): void {
-    const score = lines * 2;
-    this.numberLinesSubject.next({ lines, score });
+  public setNumberFilledLines(numberLines: number): void {
+    const initialScore = numberLines * DEFAULT_STEP;
+    this.currentNumberLines = numberLines ? this.currentNumberLines + numberLines : numberLines;
+    this.currentScore = initialScore
+      ? this.currentScore + GameService.calculateScore(numberLines, this.currentLevel)
+      : initialScore;
+    if (this.currentNumberLines >= GAME_STEP_LEVEL * this.currentLevel) {
+      this.currentLevel += 1;
+    }
+
+    const lines = this.currentNumberLines;
+    const score = this.currentScore;
+    const level = this.currentLevel;
+
+    this.numberLinesSubject.next({ lines, score, level });
   }
 
   public setInitialInformation(): void {
-    this.numberLinesSubject.next({ lines: 0, score: 0 });
+    this.currentLevel = 1;
+    this.currentScore = 0;
+    this.currentNumberLines = 0;
+
+    const lines = this.currentNumberLines;
+    const score = this.currentScore;
+    const level = this.currentLevel;
+    this.numberLinesSubject.next({ lines, score, level });
   }
 
-  public onUpdateNumberLines(): Observable<{
-    lines: number;
-    score: number;
-  }> {
+  public onUpdateGameInformation(): Observable<GameStatsObject> {
     return this.numberLinesSubject.asObservable();
   }
 }
