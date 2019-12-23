fbcdf02f1a2766091f11816ddf7633728f482101
IraCherginyshka <39628469+IraCherginyshka@users.noreply.github.com>
2019-12-23 18:14:58 +0200

    Feature/ATG-93 Common leaderboard layout (#51)
    * create a common leaderboard layout
    
    * Resolve conversation (semantics)

--- src/app/components/game-leaderboard/game-leaderboard.component.ts
+++ src/app/components/game-leaderboard/game-leaderboard.component.ts
@@ -5,4 +5,67 @@ import { Component } from '@angular/core';
   templateUrl: './game-leaderboard.component.html',
   styleUrls: ['./game-leaderboard.component.scss'],
 })
-export class GameLeaderboardComponent {}
+export class GameLeaderboardComponent {
+  public gameResults = [
+    {
+      username: 'Player1',
+      level: 333555,
+      score: 77733333,
+      lines: 500,
+    },
+    {
+      username: 'Player2',
+      level: 555333,
+      score: 77733333,
+      lines: 500,
+    },
+    {
+      username: 'Player3',
+      level: 333,
+      score: 33333000,
+      lines: 500,
+    },
+    {
+      username: 'Player4',
+      level: 336573,
+      score: 35763333,
+      lines: 500,
+    },
+    {
+      username: 'Player5',
+      level: 336573,
+      score: 35763333,
+      lines: 500,
+    },
+    {
+      username: 'Player6',
+      level: 336573,
+      score: 35763333,
+      lines: 500,
+    },
+    {
+      username: 'Player7',
+      level: 336573,
+      score: 35763333,
+      lines: 500,
+    },
+    {
+      username: 'Player8',
+      level: 336573,
+      score: 35763333,
+      lines: 500,
+    },
+    {
+      username: 'Player9',
+      level: 336573,
+      score: 35763333,
+      lines: 500,
+    },
+    {
+      username: 'Player10',
+      level: 336573,
+      score: 35763333,
+      lines: 500,
+    },
+  ];
+}
